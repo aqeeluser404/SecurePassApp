c14d2190241e5d10403b04f844aafcaf94adb348
aqeeluser404 <aqeelhanslo@gmail.com>
2024-03-08 23:20:09 +0200

    created home page and stylings

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
   <main>
-    <div class="dynamic-container flex-row">
-      <div class="text-container">
-        <h1 class="font-size-responsive-giant">Hi there</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, reiciendis?</p>
+    <div class="dynamic-container flex-row-start">
+      <div class="text-container flex-column-start">
+        <h1 class="font-size-responsive-giant text-uppercase">Fast and Secure</h1>
+        <p class="font-size-responsive-sm">Are you tired of jotting down your passwords and then losing track of them? <br> Discover the simplicity and security of our password vault solution.</p>
+        <button class="button">Get Started</button>
       </div>
     </div>
   </main>
 </template>
+
+<style scoped>
+.dynamic-container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
